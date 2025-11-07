142029d0c1ee4e1d001f93080941a48b59a2c1b0
nhminh107 <24120381@student.hcmus.edu.vn>
2025-11-07 14:42:12 +0700

    Add project files.

---
+++ Color.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Color {
+private: 
+public: 
+	int r, g, b, a; 
+	Color() {
+		r = g = b = a = -1; 
+	}
+};
+
+class Stroke {
+private: 
+	Color strokeColor; 
+	float strokeWidth; 
+public: 
+	Stroke(); 
+	~Stroke(); 
+
+	void setStrokeColor(const Color&); 
+	void setStrokeWidth(float); 
+	Color getStrokeColor() const { return strokeColor; }
+	float getStorkeWidth() const { return strokeWidth; }
+};
