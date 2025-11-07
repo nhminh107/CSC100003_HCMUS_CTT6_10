3de138d065353ad75f86afaac10490abda541f74
nhminh107 <24120381@student.hcmus.edu.vn>
2025-11-07 15:34:28 +0700

    Ahiih

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,9 @@
 #include "Libary.h"
 using namespace std;
 
-Point::Point(): x(0), y(0) {}
+Point::Point() {
+	x = 0; 
+	y = 0; 
+	intersect = false; 
+}
+Point::Point(float xPoint, float yPoint) : x(xPoint), y(yPoint), intersect(false) {};
